aa748a026726ce7fa976bcdc3228fbc53714e92f
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 15:32:28 +0300

    1.1

--- src/Noten.java
+++ src/Noten.java
@@ -1,2 +1,22 @@
 public class Noten {
+
+    public static int[] getFailingGrades(int[] grades) {
+        int count = 0;
+
+        for (int g : grades) {
+            if (g < 40)
+                count++;
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int g : grades) {
+            if (g < 40)
+                result[index++] = g;
+        }
+
+        return result;
+    }
+
 }
