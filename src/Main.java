c9b9140cfa83c805e58e668b47e60863d7a0efe4
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 15:40:54 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
+
+    static void main() {
+        int[] grades = {29, 37, 38, 41, 84, 67};
+        System.out.println(Arrays.toString(Noten.getFailingGrades(grades)));
+        System.out.println(Noten.getAverage(grades));
+        System.out.println(Arrays.toString(Noten.getRoundedGrades(grades)));
+        System.out.println(Noten.getMaxRoundedGrade(grades));
+    }
 }
